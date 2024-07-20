61860f698b3309bce113abede020664fca4e829f
Tiago Monteiro <tiago.rag@gmail.com>
2024-07-20 12:47:51 -0300

    Primeiro commit

---
+++ src/main/java/br/com/tiagomonteiro/desafiosegurotarifado/DesafiosegurotarifadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagomonteiro.desafiosegurotarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosegurotarifadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiosegurotarifadoApplication.class, args);
+	}
+
+}
